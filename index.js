75719ede78d43801fc5e06bcf51b3d14e4dcef75
Jeongmin Woo <greenblues1190@gmail.com>
2022-01-07 04:23:51 +0900

    refactor: improve test environment
    - add node env in npm script
    - extract server listen code

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
-const PORT = 3000;
 
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'dev') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const users = require('./api/users/index');
 app.use('/api/users', users);
-
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
 
 module.exports = app;
